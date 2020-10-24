3e50aea1f12dbe846d001fe742fb8cad2f0241bc
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-24 17:56:54 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+table = 7
+
+zeroTimes = 0*table
+oneTimes = 1*table
+twoTimes = 2*table
+threeTimes = 3*table
+fourTimes = 4*table
+fiveTimes = 5*table
+sixTimes = 6*table
+sevenTimes = 7*table
+eightTimes = 8*table
+nineTimes = 9*table
+tenTimes = 10*table
+
+print(zeroTimes)
+print(oneTimes)
+print(twoTimes)
+print(threeTimes)
+print(fourTimes)
+print(fiveTimes)
+print(sixTimes)
+print(sevenTimes)
+print(eightTimes)
+print(nineTimes)
+print(tenTimes)
