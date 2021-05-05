ad9c9af9faee9370710d958bd2946428b25ada24
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-05 14:08:15 +0100

    Added answer

--- main.py
+++ main.py
@@ -2,26 +2,17 @@ import numpy as np
 
 table = 7
 
-zeroTimes = 0*table
-oneTimes = 1*table
-twoTimes = 2*table
-threeTimes = 3*table
-fourTimes = 4*table
-fiveTimes = 5*table
-sixTimes = 6*table
-sevenTimes = 7*table
-eightTimes = 8*table
-nineTimes = 9*table
-tenTimes = 10*table
+timesTable = np.zeros(11)
+timesTable[0] = 0*table
+timesTable[1] = 1*table
+timesTable[2] = 2*table
+timesTable[3] = 3*table
+timesTable[4] = 4*table
+timesTable[5] = 5*table
+timesTable[6] = 6*table
+timesTable[7] = 7*table
+timesTable[8] = 8*table
+timesTable[9] = 9*table
+timesTable[10] = 10*table
 
-print(zeroTimes)
-print(oneTimes)
-print(twoTimes)
-print(threeTimes)
-print(fourTimes)
-print(fiveTimes)
-print(sixTimes)
-print(sevenTimes)
-print(eightTimes)
-print(nineTimes)
-print(tenTimes)
+print(timesTable)
